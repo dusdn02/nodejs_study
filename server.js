604d6b23dd77e0912353b62bbbe59d3d0cb0657b
dusdn02 <lyw9114@naver.com>
2020-08-10 14:33:14 +0900

    addListener 사용

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+// var http = require('http');
+
+// http.createServer(function (request, response){
+//     response.writeHead(200,{'Content-Type' : 'text/plain'});
+//     response.write('Hello nodejs');
+//     response.end();
+// }).listen(8888);
+
 var http = require('http');
 
-http.createServer(function (request, response){
-    response.writeHead(200,{'Content-Type' : 'text/plain'});
+var server = http.createServer();
+
+server.addListener('request', function (request, response) {  
+    console.log('requested...');
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
     response.write('Hello nodejs');
     response.end();
-}).listen(8888);
+});
+
+server.addListener('connection', function(socket){  
+    console.log('connected...');
+});
+
+server.listen(8888);
+//node server.js로 서버를 열고 실행시켜야함
