fe7aa1abe4d12eb308ac98cf08fc15f99a2871ac
dusdn02 <lyw9114@naver.com>
2020-08-21 19:29:38 +0900

    동기적 처리

---
+++ async_test.js
@@ -0,0 +1,12 @@
+console.log("1st");
+
+setTimeout(()=>{
+    console.log("2nd");
+}, 0)
+
+console.log("3rd");
+console.log("4th");
+console.log("5th");
+console.log("6th");
+console.log("7th");
+console.log("8th");
---
+++ sync_test.js
@@ -0,0 +1,5 @@
+console.log("1st");
+
+console.log("2nd");
+
+console.log("3rd");
